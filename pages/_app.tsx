814a31b26aebfe20b82cb1ca01a65e7ffaf869ef
Mentis123 <nft.mentis@gmail.com>
2023-04-07 00:20:02 +1000

    Fixed error

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,13 @@
 import { ThemeProvider } from '../ThemeContext';
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
 
-export default function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider>
       <Component {...pageProps} />
     </ThemeProvider>
   );
- {
-  return <Component {...pageProps} />
 }
 
 export default MyApp;
