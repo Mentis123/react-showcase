dfa9b87950d06eaedcc7970d4702e1b84e45eea9
Mentis123 <nft.mentis@gmail.com>
2023-04-07 00:29:11 +1000

    More fixes - yay

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,4 @@
-import { ThemeProvider } from '../ThemeContext';
+import { ThemeProvider } from '../contexts/ThemeContext';
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 
